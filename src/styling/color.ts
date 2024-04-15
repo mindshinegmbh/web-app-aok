6eac2563242f4c461e3eef557f450a0c4d6c33d5
dooamindshine <dooa@mindshine.app>
2024-04-15 19:04:38 +0200

    wip(darkmode): themes and color object added

---
+++ src/styling/color.ts
@@ -0,0 +1,12 @@
+const Colors = {
+  green_shade_0: '#015e40',
+  green_shade_1: '#17ab42',
+  green_shade_2: '#0e3a2d',
+  green_shade_3: '#7fe249',
+  green_shade_4: '#91f549',
+  black_shade_0: '#00150f',
+  white_shade_0: '#ffffff',
+  grey_shade_0: '#5f6466',
+};
+
+export default Colors;
---
+++ src/styling/themes.ts
@@ -0,0 +1,22 @@
+import Colors from './color';
+
+const Themes = {
+  light: {
+    screen_background: Colors.green_shade_0,
+    element_background: Colors.white_shade_0,
+    primary_text_color: Colors.white_shade_0,
+    secondary_text_color: Colors.grey_shade_0,
+    settings_text_color: Colors.green_shade_3,
+    header_text_color: Colors.green_shade_1,
+  },
+  dark: {
+    screen_background: Colors.black_shade_0,
+    element_background: Colors.green_shade_2,
+    primary_text_color: Colors.white_shade_0,
+    secondary_text_color: Colors.white_shade_0,
+    settings_text_color: Colors.green_shade_4,
+    header_text_color: Colors.green_shade_4,
+  },
+};
+
+export default Themes;
