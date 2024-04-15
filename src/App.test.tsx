bf7685ccf4bfcd057e1431858109ecebd4c3e45e
dooamindshine <dooa@mindshine.app>
2024-04-15 16:55:07 +0200

    feat(darkmode): initializing new feature dark mode

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { setupStore } from './redux/store';
-import { renderWithProviders } from './utils/test_utils';
+import { addI18nResources, renderWithProviders } from './utils/test_utils';
 import { initI18n } from './utils/test_utils';
 
 beforeAll(() => {
@@ -11,6 +11,7 @@ beforeAll(() => {
 
 test('renders learn react link', () => {
   const store = setupStore();
+  addI18nResources({ home: { heading: 'Learn React' } });
   renderWithProviders(<App />, { store });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
