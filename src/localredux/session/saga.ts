10d78ab1db1dfd84e9d71ad017cca4c6157d9e21
dooamindshine <dooa@mindshine.app>
2024-05-15 16:05:22 +0200

    feat(pager): pager added, some fixes also done

--- src/localredux/session/saga.ts
+++ src/localredux/session/saga.ts
@@ -11,6 +11,7 @@ export function* getSession({
   type: typeof GET_SESSION_ACTION;
   payload: GetSession;
 }) {
+  console.log(type)
   const { data } = yield call(apis.getSessionRequest, payload.sessionId);
   yield put(setSessionData(data[0]?.session_v3));
   yield delay(3000)
